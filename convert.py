7b01eff52b2b92d50857287859349d6091a64688
arisuandlainkissingcompetition123456789 <z@doxbin.email>
2022-11-30 05:27:10 +0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,11 +1,13 @@
 import sys
 def convert_code(code: str):
+    CHARACTERS = "owo "
+    DELIMITER = "⠀" # set this in run.py to the .split() method
     string = ""
     for line in code.splitlines():
         line.strip('\n')
         for character in line:
-            string += str("owo " * ord(character))[:ord(character)] + "⠀"
-        string += str("owo " * ord("\n"))[:ord("\n")] + "⠀"
+            string += str(CHARACTERS * ord(character))[:ord(character)] + DELIMITER
+        string += str(CHARACTERS * ord("\n"))[:ord("\n")] + DELIMITER
     with open("owo.owo", 'w', encoding='utf-8') as owo:
         owo.write(string.strip()[:len(string.strip())-1])
 convert_code(open(sys.argv[1], encoding='utf-8').read())
